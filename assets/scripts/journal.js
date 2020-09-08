be9c73582a12ba538552a096ae8d784e2533073e
BrandonHillbom <bahillbom@gmail.com>
2020-09-08 11:06:13 -0600

    styling and muscle groups scripts

--- assets/scripts/journal.js
+++ assets/scripts/journal.js
@@ -1,28 +1,64 @@
 window.onload = function () {
     const entries = [
         {
-            name: 'Workout',
-            created: new Date(),
+            name: 'Workout 1',
+            created: new Date('2020-07-30'),
             muscleGroups: ['Hamstrings', 'Calves', 'Quads', 'Glutes'],
             excerises: ['Barbell squat', 'Barbell Sumo Squat', 'Hack Squat', 'Smith Machine Hip Thrusts', 'Hamstring Machine Curls', 'Smith Machine Calf Raises', 'Calf Extensions'],
-            sets: '',
-            reps: ''
+            sets: 4,
+            reps: 10
         },
         {
             name: 'Workout 2',
-            created: new Date(),
+            created: new Date('2020-08-01'),
             muscleGroups: ['chest', 'Triceps', 'Shoulders'],
             excerises: ['Barbell Bench Press'],
-            sets: '',
-            reps: ''
+            sets: 4,
+            reps: 12
         },
     ]
+    const journalEntriesEl = document.getElementById('journal-entries')
+    journalEntriesEl.innerHTML = ''
 
     entries.forEach(function (entry) {
-        const paragraph = document.querySelector('#journal1');
-        paragraph.innerText = `${entry.name} \n sets: ${entry.sets} \n reps: ${entry.reps}`
+        const entryEl = document.createElement('div')
 
-        const SecondParagraph = document.querySelector('#journal2');
-        SecondParagraph.innerText = entry.created.toLocaleString()
+        const titleEl = document.createElement('h2')
+        titleEl.innerText = entry.name
+        entryEl.appendChild(titleEl)
+
+        const dateEl = document.createElement('p')
+        dateEl.innerText = entry.created.toLocaleDateString()
+        entryEl.appendChild(dateEl)
+
+        const muscleGroupsEl = document.createElement('div')
+        const muscleGroupsHeadingEl = document.createElement('h3')
+        muscleGroupsHeadingEl.innerText = 'Muscle Groups'
+        muscleGroupsEl.appendChild(muscleGroupsHeadingEl)
+        const muscleGroupsListEl = document.createElement('ul')
+
+        entry.muscleGroups.forEach(function (muscleGroup) {
+            const muscleGroupListItemEl = document.createElement('li')
+            muscleGroupListItemEl.innerText = muscleGroup
+            muscleGroupsListEl.appendChild(muscleGroupListItemEl)
+
+        })
+        muscleGroupsEl.appendChild(muscleGroupsListEl)
+        entryEl.appendChild(muscleGroupsEl)
+
+
+        const exercisesEl = document.createElement('p')
+        exercisesEl.innerText = entry.excerises
+        entryEl.appendChild(exercisesEl)
+
+        const setEl = document.createElement('p')
+        setEl.innerText = `Sets: ${entry.sets}`
+        entryEl.appendChild(setEl)
+
+        const repEl = document.createElement('p')
+        repEl.innerText = `Reps: ${entry.reps}`
+        entryEl.appendChild(repEl)
+
+        journalEntriesEl.appendChild(entryEl)
     })
 }
